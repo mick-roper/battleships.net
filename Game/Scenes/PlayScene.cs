722b1a3ec50b9d09a515e27f49b651848ef515e5
Mick Roper <michael.roper@drax.com>
2018-02-13 11:58:17 +0000

    updating the play scene class

--- Game/Scenes/PlayScene.cs
+++ Game/Scenes/PlayScene.cs
@@ -7,10 +7,19 @@ namespace Battleship.Scenes
     class PlayScene : Scene
     {
         readonly Player player;
+        readonly Player computer;
+
+        SceneState state;
+        string input;
+
+        int playerShots = 0, compShots = 0, playerHits = 0, compHits = 0;
 
         public PlayScene(Game game, Player player) : base(game)
         {
             this.player = player ?? throw new ArgumentNullException(nameof(player));
+            this.computer = new Player("Computer");
+
+            state = SceneState.NotInitialised;
         }
 
         protected override void DrawScene(IRenderer renderer)
@@ -20,12 +29,33 @@ namespace Battleship.Scenes
 
         public override void HandleInput(IInputHandler inputHandler)
         {
-            throw new NotImplementedException();
+            if (state > SceneState.NotInitialised)
+            {
+                input = inputHandler.ReadInput();
+            }
         }
 
         public override void Update()
         {
-            throw new NotImplementedException();
+            switch (state)
+            {
+                default:
+                case SceneState.NotInitialised:
+                    {
+                        state = SceneState.PlayerPlacePieces;
+                    }
+                    break;
+            }
+        }
+
+        enum SceneState
+        {
+            NotInitialised,
+            PlayerPlacePieces,
+            ComputerPlacePieces,
+            PlayerTurnToShoot,
+            ComputerTurnToShoot,
+            GameOver
         }
     }
 }
