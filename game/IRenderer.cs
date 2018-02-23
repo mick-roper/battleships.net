d13e8e19a64040ac56fb22875ed285f3cced6b6c
mick-roper <mick@theropers.co.uk>
2018-02-23 09:22:26 +0000

    added spash scene

--- game/IRenderer.cs
+++ game/IRenderer.cs
@@ -6,6 +6,25 @@ namespace Battleships
 {
     interface IRenderer
     {
-        void Draw(char c, int x, int y);
+        /// <summary>
+        /// Gets the width of the rendering surface
+        /// </summary>
+        int Width { get; }
+        /// <summary>
+        /// Gets the height of the rendering surface
+        /// </summary>
+        int Height { get; }
+        /// <summary>
+        /// Draws a character to the screen
+        /// </summary>
+        /// <param name="character">The character to draw</param>
+        /// <param name="x">The x coord</param>
+        /// <param name="y">The y coord</param>
+        void Draw(char character, int x, int y);
+        /// <summary>
+        /// Sets the cursor visibility
+        /// </summary>
+        /// <param name="visible"><c>true</c> to show the cursor, else <c>false</c></param>
+        void ShowCursor(bool visible);
     }
 }
--- game/Scenes/SplashScene.cs
+++ game/Scenes/SplashScene.cs
@@ -6,23 +6,39 @@ namespace Battleships.Scenes
 {
     class SplashScene : Scene
     {
+        const int MAX_TICKS = 25;
+        const string SPLASH_MESSAGE = "Battleships!";
+
         public SplashScene(IGame game) : base(game)
         {
         }
 
         public override void HandleInput(IInputService inputService)
         {
-            throw new NotImplementedException();
+            return; // nothing to handle
         }
 
         public override void Render(IRenderer renderer)
         {
-            throw new NotImplementedException();
+            renderer.ShowCursor(false);
+
+            int messageLength = SPLASH_MESSAGE.Length;
+            int x, y;
+
+            x = (renderer.Width / 2) + (messageLength / 2);
+            y = 1;
+
+            for (int i = 0; i < messageLength; i++)
+            {
+                renderer.Draw(SPLASH_MESSAGE[i], x += i, y);
+            }
         }
 
         public override void Update(int ticks)
         {
-            throw new NotImplementedException();
+            // todo: transition to other scene after ticks have elapsed
+
+            return;
         }
     }
 }
