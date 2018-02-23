0862de9da5a1823719512ee611acca44f5436615
mick-roper <mick@theropers.co.uk>
2018-02-23 10:23:22 +0000

    Added all scenes - they need implementing but they're here!

---
+++ game/Scenes/GameOverScene.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Battleships.Scenes
+{
+    class GameOverScene : Scene
+    {
+        public GameOverScene(IGame game) : base(game)
+        {
+        }
+
+        public override void HandleInput(IInputService inputService)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Render(IRenderer renderer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
