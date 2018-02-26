86631a95cb5c69f832ea3e0badb52084e07d699c
mick-roper <mick@theropers.co.uk>
2018-02-26 13:10:55 +0000

    Added piece enum

---
+++ game/Entities/Piece.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Battleships.Entities
+{
+    public enum Piece : byte
+    {
+        Destroyer,
+        Frigate,
+        Cruiser,
+        Battleship,
+        Carrier
+    }
+}
